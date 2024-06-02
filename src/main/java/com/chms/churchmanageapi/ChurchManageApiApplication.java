45b564e877a7e51d8e3775fc3690d5c1917014a2
roshanpradhan <pradhanr@vcu.edu>
2024-06-02 15:05:50 -0400

    Initial Commit For CHMSAPI

---
+++ src/main/java/com/chms/churchmanageapi/ChurchManageApiApplication.java
@@ -0,0 +1,13 @@
+package com.chms.churchmanageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchManageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchManageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chms/churchmanageapi/ChurchManageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chms.churchmanageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchManageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
