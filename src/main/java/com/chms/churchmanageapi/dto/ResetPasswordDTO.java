92fb4eeb49b0b9dac58aed4398207a309db7862b
roshanpradhan <pradhanr@vcu.edu>
2024-09-29 01:35:47 -0400

    dtos push

---
+++ src/main/java/com/chms/churchmanageapi/dto/ResetPasswordDTO.java
@@ -0,0 +1,14 @@
+package com.chms.churchmanageapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordDTO {
+    private String username;
+    private String password;
+    private String email;
+}
